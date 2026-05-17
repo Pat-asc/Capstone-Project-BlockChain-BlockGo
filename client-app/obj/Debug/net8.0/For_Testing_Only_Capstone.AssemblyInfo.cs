41c0d8ca3c69fc80a3f0705896fbb2e3dcd63862
Cookies-Dookies <pascualpatrick264@gmail.com>
2026-05-17 19:10:37 +0800

    fix 18

--- client-app/obj/Debug/net8.0/For_Testing_Only_Capstone.AssemblyInfo.cs
+++ client-app/obj/Debug/net8.0/For_Testing_Only_Capstone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("For_Testing_Only_Capstone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a131b02e87458875e5757c700d8624fe73283ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("For_Testing_Only_Capstone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("For_Testing_Only_Capstone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
